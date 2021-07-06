5d62b7b786a9c31f1ebf436a80e52e0ef75f7d8b
matevz.sraml <matevz.sraml@birkle-it.com>
2021-07-06 13:55:23 +0200

    postgres added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from security import authenticate, identity
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -7,11 +8,10 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'matevz'
 api = Api(app)
