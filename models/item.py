4cf179f3885e8512f5c2a317590ee9e8110eb759
matevz.sraml <matevz.sraml@birkle-it.com>
2021-07-06 13:35:44 +0200

    bug fix

--- models/item.py
+++ models/item.py
@@ -7,7 +7,7 @@ class ItemModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
-    store_id = db.Column(db.Integer, db.models.ForeignKey("stores.id", verbose_name=_(""), on_delete=models.CASCADE))
+    store_id = db.Column(db.Integer, db.ForeignKey("stores.id"))
     store = db.relationship('StoreModel')
     
     def __init__(self, name, price):
